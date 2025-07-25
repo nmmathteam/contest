8a468f3462ced9bd7e72f57d7f93817c2c3a06a3
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-07-24 22:22:54 -0600

    Delete _app directory

--- _app/immutable/entry/start.zZtGmzFy.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/njT0hsQR.js";export{o as load_css,r as start};
--- _app/immutable/nodes/1.BpxKB4Th.js
+++
@@ -1,9 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/CiFSUvZa.js";import{n as h,t as o,D as c,b as r,o as f,$ as g,ap as u,s as v,z as _,Y as y}from"../chunks/sgvucTjU.js";import{h as b}from"../chunks/CFrKh5KW.js";import{i as k}from"../chunks/Dbzdn9Az.js";import{s as x,p as a}from"../chunks/njT0hsQR.js";import{N as $}from"../chunks/BzzbzXmb.js";import{H as w}from"../chunks/qNSAn6RH.js";const N={get error(){return a.error},get status(){return a.status}};x.updated.check;const s=N;var H=o(`<meta name="description" content=""> <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet"> <style>header {
-            height: 100vh !important;
-        }
-        header h1 {
-            margin-bottom: 200px;
-        }
-        body {
-            overflow: hidden;
-        }</style>`,1),M=o("<!> <!>",1);function C(n,p){h(p,!1),k();var t=M();b(d=>{var l=H();g.title="NM³ | Sponsors",u(4),r(d,l)});var e=c(t);$(e,{clr:"dark"});var i=v(e,2);const m=y(()=>s.status+" "+s.error.message);w(i,{get title(){return _(m)}}),r(n,t),f()}export{C as component};
