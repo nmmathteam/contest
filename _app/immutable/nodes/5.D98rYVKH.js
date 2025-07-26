ad9dcd4b033dee977c7b49f7d2657b92a68b6a04
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-07-25 20:47:40 -0600

    Add files via upload

---
+++ _app/immutable/nodes/5.D98rYVKH.js
@@ -0,0 +1,17 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/CK3zJ1kb.js";import{t as o,T as f,a as t,$ as g,k as v,s as a,l as y}from"../chunks/CyzUBJTW.js";import{h as w}from"../chunks/BpTedM5O.js";import{N as b}from"../chunks/Bosby5WN.js";import{F as x}from"../chunks/C8ZVisFr.js";import{H as _}from"../chunks/BP1tx3br.js";import{G as d}from"../chunks/B3sMh9t_.js";var $=o('<meta name="description" content="">'),N=o(`<ul class="space-y-1 text-gray-800 list-disc max-w-4xl mt-7 mx-auto"><li>Each team can have no more than <span class="font-bold">4</span> people.
+            Smaller teams will be at a disadvantage.</li> <li>Communication within teams will be permitted during every round.
+            However, you may not access the internet or communicate with other
+            teams or coaches during any exam.</li> <li>No aids other than writing instruments and paper will be allowed.
+            Rulers and compasses <span class="font-bold">may</span> be used on the contest. However, calculators, protractors, graph paper,
+            notes, and other similar aids <span class="font-bold">may not</span> be used.</li> <li>You <span class="font-bold">may not</span> use any communication devices
+            such as cell phones and computers during any exam.</li> <li>If a proctor has reasonable suspicion to believe that a student is
+            cheating, the proctor reserves the right to declare cheating.</li> <li>If a student or team is caught cheating, their team will get a -X
+            for that round, where X is the score of the highest scoring team on
+            that round.</li> <li>NM&sup3; will use the mathematical conventions found <a href="/">here</a>. Please familiarize yourself with these conventions before the
+            competition if you can!</li></ul>`),S=o(`<ul class="space-y-1 text-gray-800 list-disc max-w-4xl mt-7 mx-auto"><li>Scoring details for the mystery round will be presented on contest
+            day.</li> <li>The guts round will consist of 8 sets of 3 problems.</li> <li>Combined scores on the mystery and guts rounds will determine
+            seeding for the countdown round. Only the top 8 teams will advance
+            to the countdown round.</li> <li>The mystery and guts rounds will each contribute to 50% of a team's
+            final score. Countdown round participants will recieve extra points
+            determined on final countdown round placement.</li> <li>If you believe we made an error, either regarding grading or
+            regarding one of the problems, you may protest by emailing us at <a href="mailto:nmmathteam@gmail.com">nmmathteam@gmail.com</a>.</li></ul>`),H=o('<section id="mainheader" class="svelte-11fqhqc"><!> <!></section> <div class="mx-7 mb-7 py-5 px-7 bg-red-100 rounded-xl fixed z-[10000] bottom-0 shadow-xl">This section is still under construction.</div> <!> <!> <!>',1);function z(u){var n=H();w(e=>{var r=$();g.title="NM³ | Rules",t(e,r)});var i=f(n),l=v(i);b(l,{clr:"dark"});var h=a(l,2);_(h,{title:"Rules & Scoring"}),y(i);var m=a(i,4);d(m,{title:"General Rules",children:(e,r)=>{var s=N();t(e,s)},$$slots:{default:!0}});var c=a(m,2);d(c,{title:"Scoring",children:(e,r)=>{var s=S();t(e,s)},$$slots:{default:!0}});var p=a(c,2);x(p),t(u,n)}export{z as component};
