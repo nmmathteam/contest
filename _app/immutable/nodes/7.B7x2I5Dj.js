8cac9269c5eb374180ef1ef12129f5ffb62056da
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-07-27 21:10:10 -0600

    campersand

---
+++ _app/immutable/nodes/7.B7x2I5Dj.js
@@ -0,0 +1,25 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/CK3zJ1kb.js";import{t as $,j,a as i,s,k as t,l as r,T as J,$ as L,ap as m,aq as d}from"../chunks/CyzUBJTW.js";import{h as P}from"../chunks/BpTedM5O.js";import{N as w}from"../chunks/Bosby5WN.js";import{F as T}from"../chunks/C8ZVisFr.js";import{H as y}from"../chunks/BP1tx3br.js";import{s as q}from"../chunks/D0RF8JLY.js";import{s as N}from"../chunks/ChPSbquv.js";/* empty css                */var F=$('<div class="card svelte-1rookjo"><div class="card-inner bg-slate-200 shadow-sm hover:shadow-xl svelte-1rookjo"><div class="ur-wrapper svelte-1rookjo"><div class="username text-gray-700 text-4xl svelte-1rookjo"> </div> <div class="role text-gray-500 text-lg"> </div></div> <div class="description text-gray-600 text-base svelte-1rookjo"><!></div></div></div>');function c(_,o){var n=F(),v=t(n),p=t(v),l=t(p),x=t(l,!0);r(l);var g=s(l,2),h=t(g,!0);r(g),r(p);var f=s(p,2),b=t(f);N(b,o,"default",{}),r(f),r(v),r(n),j(()=>{q(x,o.name),q(h,o.role)}),i(_,n)}var H=$('<meta name="description" content="">'),O=$('<section id="mainheader" class="svelte-gc1q16"><!> <!></section> <div class="mx-7 mb-7 py-5 px-7 bg-red-100 rounded-xl fixed z-[10000] bottom-0 shadow-xl">This section is still under construction.</div> <section class="card-wrapper mt-20 svelte-gc1q16"><!> <!> <!> <!> <!> <!></section> <!>',1);function R(_){var o=O();P(e=>{var u=H();L.title="NM³ | Our Team",i(e,u)});var n=J(o),v=t(n);w(v,{clr:"dark"});var p=s(v,2);y(p,{title:"Our Team"}),r(n);var l=s(n,4),x=t(l);c(x,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}});var g=s(x,2);c(g,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}});var h=s(g,2);c(h,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}});var f=s(h,2);c(f,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}});var b=s(f,2);c(b,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}});var D=s(b,2);c(D,{name:"John Doe",role:"Director",children:(e,u)=>{m();var a=d(`Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus
+        ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus
+        duis convallis tempus leo eu aenean sed diam urna tempor pulvinar
+        vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl
+        malesuada lacinia integer nunc posuere ut hendrerit.`);i(e,a)},$$slots:{default:!0}}),r(l);var k=s(l,2);T(k),i(_,o)}export{R as component};
