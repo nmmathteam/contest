10d2bea4d7641c9155b7579c0bed2df17b3c356c
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-08-02 11:20:28 -0600

    remove all warnings and launch

---
+++ _app/immutable/nodes/1.Ck9OyC1_.js
@@ -0,0 +1,10 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/CK3zJ1kb.js";import{p as c,t as o,T as g,a as r,b as h,$ as f,ap as u,s as v,g as b,Y as _}from"../chunks/CyzUBJTW.js";import{h as k}from"../chunks/BpTedM5O.js";import{i as y}from"../chunks/BU120lST.js";import{s as x,p as a}from"../chunks/DBwxshFG.js";import{N as $}from"../chunks/Bosby5WN.js";import{H as w}from"../chunks/BP1tx3br.js";const N={get error(){return a.error},get status(){return a.status}};x.updated.check;const s=N;var H=o(`<meta name="description" content=""> <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet"> <style>header {
+            height: 100vh !important;
+            background: black;
+        }
+        header h1 {
+            margin-bottom: 200px;
+        }
+        body {
+            overflow: hidden;
+        }</style>`,1),M=o("<!> <!>",1);function C(p,n){c(n,!1),y();var t=M();k(d=>{var l=H();f.title="NM³ | Sponsors",u(4),r(d,l)});var e=g(t);$(e,{clr:"dark"});var i=v(e,2);const m=_(()=>s.status+" "+s.error.message);w(i,{get title(){return b(m)}}),r(p,t),h()}export{C as component};
