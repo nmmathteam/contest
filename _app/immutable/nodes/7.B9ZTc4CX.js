7ee4a4c763e082a6f67a958f57115ce8d113b946
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-08-10 10:23:11 -0600

    new favicon

---
+++ _app/immutable/nodes/7.B9ZTc4CX.js
@@ -0,0 +1,18 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/DQ977XPr.js";import{t as x,k as S,a as r,s as t,l as o,m as n,h as A,V as w,$ as b,ap as u,aq as p}from"../chunks/BTWhDYjr.js";import{N as P}from"../chunks/CM1yisVM.js";import{F as H}from"../chunks/DegMRG_z.js";import{H as k}from"../chunks/B9gSgS-v.js";import{s as $}from"../chunks/IRzFx_1A.js";import{s as C}from"../chunks/DdpjTra2.js";/* empty css                */var j=x('<div class="card svelte-1rookjo"><div class="card-inner bg-slate-200 shadow-sm hover:shadow-xl svelte-1rookjo"><div class="ur-wrapper svelte-1rookjo"><div class="username text-gray-700 text-4xl svelte-1rookjo"> </div> <div class="role text-gray-500 text-lg"> </div></div> <div class="description text-gray-600 text-base svelte-1rookjo"><!></div></div></div>');function f(y,l){var s=j(),c=o(s),h=o(c),i=o(h),g=o(i,!0);n(i);var m=t(i,2),M=o(m,!0);n(m),n(h);var v=t(h,2),_=o(v);C(_,l,"default",{}),n(v),n(c),n(s),S(()=>{$(g,l.name),$(M,l.role)}),r(y,s)}var q=x('<meta name="description" content="">'),O=x('<section id="mainheader" class="svelte-gc1q16"><!> <!></section> <section class="card-wrapper mt-20 svelte-gc1q16"><!> <!> <!> <!> <!></section> <!>',1);function L(y){var l=O();A(e=>{var d=q();b.title="NM³ | Our Team",r(e,d)});var s=w(l),c=o(s);P(c,{clr:"dark"});var h=t(c,2);k(h,{title:"Our Team"}),n(s);var i=t(s,2),g=o(i);f(g,{name:"Sean Choi",role:"Director",children:(e,d)=>{u();var a=p(`Sean is a rising senior attending Stanford Online High School. He
+        currently contributes as a coach for the UNM-PNM contest alongside being
+        the director of the New Mexico Math Meet. Sean is also a 4-time AIME
+        qualifier as well as a UNM-PNM winner. Aside from math, Sean enjoys
+        playing the piano, reading philosophy, and watching soccer.`);r(e,a)},$$slots:{default:!0}});var m=t(g,2);f(m,{name:"Grace Hsieh",role:"Staff",children:(e,d)=>{u();var a=p(`Hi, my name is Grace Hsieh. I'm going to junior year at LaCueva high
+        school and my favorite math concepts are knots, calculus, geometry, and
+        algebra. I went to Mathcounts Nationals in 2023 and have participated in
+        UNM PNM since 2020 in addition to coaching Mathcounts at Desert Ridge
+        and the Math Bashers club for elementary schoolers in Albuquerque. In my
+        free time, I like to play piano, do crosswords, read, and draw.`);r(e,a)},$$slots:{default:!0}});var M=t(m,2);f(M,{name:"Alyssa Sun",role:"Staff",children:(e,d)=>{u();var a=p(`Alyssa was a 2023-24 Young Women in Mathematics Winner and received an
+        Honorable Mention in the 2024-25 UNM-PNM Math Contest. In her free time,
+        she enjoys designing hand-lettered gifts and stickers.`);r(e,a)},$$slots:{default:!0}});var v=t(M,2);f(v,{name:"Aiden Kim",role:"Staff",children:(e,d)=>{u();var a=p(`Aiden (Euiryeon) Kim has competed in several math competitions,
+        including MATHCOUNTS, the UNM-PNM Math Contest, and the AMC. In his free
+        time, he listens to music, watches K-Dramas, and plays video games.`);r(e,a)},$$slots:{default:!0}});var _=t(v,2);f(_,{name:"Alicia Peng",role:"Staff",children:(e,d)=>{u();var a=p(`Alicia Peng is a current Junior at La Cueva High School, and has
+        previously competed in several math competitions such as AMC,
+        MATHCOUNTS, and KSEA. Outside of math, she also participates in several
+        other programs and clubs such as the Albuquerque Youth Symphony, Science
+        Olympiad, and National Youth Orchestra 2.`);r(e,a)},$$slots:{default:!0}}),n(i);var N=t(i,2);H(N),r(y,l)}export{L as component};
