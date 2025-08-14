767711445013f593d7b98909c41354643ecd4b76
Sean <69376044+sjchoi25@users.noreply.github.com>
2025-08-13 20:31:10 -0600

    testing meta for google relevance

---
+++ _app/immutable/nodes/5.Tfr8tbl3.js
@@ -0,0 +1,17 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/DQ977XPr.js";import{t as r,h as f,V as g,a as s,$ as w,l as v,s as t,m as y}from"../chunks/BTWhDYjr.js";import{N as b}from"../chunks/CM1yisVM.js";import{F as x}from"../chunks/DegMRG_z.js";import{H as $}from"../chunks/B9gSgS-v.js";import{G as m}from"../chunks/C-Lo__Ln.js";var _=r(`<ul class="space-y-1 text-gray-800 list-disc max-w-4xl mt-7 mx-auto px-10"><li>Each team can have no more than <span class="font-bold">4</span> people.
+            Smaller teams will be at a disadvantage.</li> <li>Communication within teams will be permitted during every round.
+            However, you may not access the internet or communicate with other
+            teams, coaches, or parents during any exam.</li> <li>No aids other than writing instruments and paper will be allowed.
+            Rulers and compasses <span class="font-bold">may</span> be used on the contest. However, calculators, protractors, graph paper,
+            notes, and other similar aids <span class="font-bold">may not</span> be used.</li> <li>You <span class="font-bold">may not</span> use any communication devices
+            such as cell phones and computers during any exam.</li> <li>If a proctor has reasonable suspicion to believe that a student is
+            cheating, the proctor reserves the right to declare cheating.</li> <li>If a student or team is caught cheating, their team will get a -X
+            for that round, where X is the score of the highest scoring team on
+            that round.</li></ul>`),S=r(`<ul class="space-y-1 text-gray-800 list-disc max-w-4xl mt-7 px-10 mx-auto"><li>Scoring details for the mystery round will be presented on contest
+            day.</li> <li>The guts round will consist of 8 sets of 3 problems. Each set will
+            be worth more points than the last.</li> <li>Combined scores on the mystery and guts rounds will determine
+            seeding for the countdown round. Only the top 8 teams will advance
+            to the countdown round.</li> <li>The mystery and guts rounds will each contribute to 50% of a team's
+            final score. Countdown round participants will recieve extra points
+            determined on final countdown round placement.</li> <li>If you believe we made an error in grading or in one of the
+            problems, feel free to email us at <a href="mailto:nmmathteam@gmail.com">nmmathteam@gmail.com</a>.</li></ul>`),H=r('<section id="mainheader" class="svelte-11fqhqc"><!> <!></section> <!> <!> <!>',1);function F(d){var n=H();f(e=>{w.title="NM³ | Rules"});var a=g(n),i=v(a);b(i,{clr:"dark"});var u=t(i,2);$(u,{title:"Rules & Scoring"}),y(a);var l=t(a,2);m(l,{title:"General Rules",children:(e,p)=>{var o=_();s(e,o)},$$slots:{default:!0}});var c=t(l,2);m(c,{title:"Scoring",children:(e,p)=>{var o=S();s(e,o)},$$slots:{default:!0}});var h=t(c,2);x(h),s(d,n)}export{F as component};
---
+++ _app/immutable/nodes/7.Ckqzgw6Y.js
@@ -0,0 +1,22 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/DQ977XPr.js";import{t as x,k as A,a as s,s as t,l as r,m as n,h as w,V as P,$ as C,ap as d,aq as c}from"../chunks/BTWhDYjr.js";import{N as b}from"../chunks/CM1yisVM.js";import{F as k}from"../chunks/DegMRG_z.js";import{H}from"../chunks/B9gSgS-v.js";import{s as _}from"../chunks/IRzFx_1A.js";import{s as j}from"../chunks/DdpjTra2.js";/* empty css                */var O=x('<div class="card svelte-1rookjo"><div class="card-inner bg-slate-200 shadow-sm hover:shadow-xl svelte-1rookjo"><div class="ur-wrapper svelte-1rookjo"><div class="username text-gray-700 text-4xl svelte-1rookjo"> </div> <div class="role text-gray-500 text-lg"> </div></div> <div class="description text-gray-600 text-base svelte-1rookjo"><!></div></div></div>');function h($,l){var o=O(),m=r(o),v=r(m),i=r(v),g=r(i,!0);n(i);var u=t(i,2),M=r(u,!0);n(u),n(v);var p=t(v,2),y=r(p);j(y,l,"default",{}),n(p),n(m),n(o),A(()=>{_(g,l.name),_(M,l.role)}),s($,o)}var q=x('<section id="mainheader" class="svelte-gc1q16"><!> <!></section> <section class="card-wrapper mt-20 svelte-gc1q16"><!> <!> <!> <!> <!> <!></section> <!>',1);function F($){var l=q();w(e=>{C.title="NM³ | Our Team"});var o=P(l),m=r(o);b(m,{clr:"dark"});var v=t(m,2);H(v,{title:"Our Team"}),n(o);var i=t(o,2),g=r(i);h(g,{name:"Sean Choi",role:"Director",children:(e,f)=>{d();var a=c(`Sean is a rising senior attending Stanford Online High School. He
+        currently contributes as a coach for the UNM-PNM contest alongside being
+        the director of the New Mexico Math Meet. Sean is also a 4-time AIME
+        qualifier as well as a UNM-PNM winner. Aside from math, Sean enjoys
+        playing the piano, reading philosophy, and watching soccer.`);s(e,a)},$$slots:{default:!0}});var u=t(g,2);h(u,{name:"Grace Hsieh",role:"Staff",children:(e,f)=>{d();var a=c(`Hi, my name is Grace Hsieh. I'm going to junior year at LaCueva high
+        school and my favorite math concepts are knots, calculus, geometry, and
+        algebra. I went to Mathcounts Nationals in 2023 and have participated in
+        UNM PNM since 2020 in addition to coaching Mathcounts at Desert Ridge
+        and the Math Bashers club for elementary schoolers in Albuquerque. In my
+        free time, I like to play piano, do crosswords, read, and draw.`);s(e,a)},$$slots:{default:!0}});var M=t(u,2);h(M,{name:"Alyssa Sun",role:"Staff",children:(e,f)=>{d();var a=c(`Alyssa was a 2023-24 Young Women in Mathematics Winner and received an
+        Honorable Mention in the 2024-25 UNM-PNM Math Contest. In her free time,
+        she enjoys designing hand-lettered gifts and stickers.`);s(e,a)},$$slots:{default:!0}});var p=t(M,2);h(p,{name:"Aiden Kim",role:"Staff",children:(e,f)=>{d();var a=c(`Aiden (Euiryeon) Kim has competed in several math competitions,
+        including MATHCOUNTS, the UNM-PNM Math Contest, and the AMC. In his free
+        time, he listens to music, watches K-Dramas, and plays video games.`);s(e,a)},$$slots:{default:!0}});var y=t(p,2);h(y,{name:"Alicia Peng",role:"Staff",children:(e,f)=>{d();var a=c(`Alicia Peng is a current Junior at La Cueva High School, and has
+        previously competed in several math competitions such as AMC,
+        MATHCOUNTS, and KSEA. Outside of math, she also participates in several
+        other programs and clubs such as the Albuquerque Youth Symphony, Science
+        Olympiad, and National Youth Orchestra 2.`);s(e,a)},$$slots:{default:!0}});var N=t(y,2);h(N,{name:"Ada Dyer",role:"Staff",children:(e,f)=>{d();var a=c(`Ada is a rising sophomore who's dual enrolled at CNM. She's participated
+        in the UNM-PNM Math Contest several times, and co-teaches virtual
+        computer science and math classes for middle schoolers with Oak Ridge
+        Computer Science Girls. When she's not studying math, she enjoys
+        learning foreign languages, watching films, and oversleeping.`);s(e,a)},$$slots:{default:!0}}),n(i);var S=t(i,2);k(S),s($,l)}export{F as component};
