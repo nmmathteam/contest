903afaf6d82586f0de7b44694ba0e8224516fcd6
euiryka <euiry.kim@gmail.com>
2026-08-24 20:30:27 -0600

    Updated sponsors and registration deadline

--- _app/immutable/entry/start.D3PFLyHW.js
+++
@@ -1,1 +0,0 @@
-import{l as o,b as r}from"../chunks/C-K_NvGC.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/1.CYFmWQG_.js
@@ -0,0 +1,10 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/CQkPEerr.js";import{p as f,E as d,a as t,b as h,f as i,a7 as g,e as c,$ as u,s as v,i as _,a0 as b}from"../chunks/Cfv17Hqi.js";import{h as k}from"../chunks/zth3F2Ci.js";import{i as y}from"../chunks/CR1h3FEw.js";import{s as $,p as s}from"../chunks/yq6oYSrz.js";import{N as w}from"../chunks/CvNk_dxi.js";import{H as x}from"../chunks/yxVdtm01.js";const N={get error(){return s.error},get status(){return s.status}};$.updated.check;const o=N;var H=i(`<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet"/> <style>header {
+            height: 100vh !important;
+            background: black;
+        }
+        header h1 {
+            margin-bottom: 200px;
+        }
+        body {
+            overflow: hidden;
+        }</style>`,1),M=i("<!> <!>",1);function D(p,m){f(m,!1),y();var a=M();k("1j96wlh",r=>{var l=H();g(2),c(()=>{u.title="NM³ | Sponsors"}),t(r,l)});var e=d(a);w(e,{clr:"dark"});var n=v(e,2);{let r=b(()=>o.status+" "+o.error.message);x(n,{get title(){return _(r)}})}t(p,a),h()}export{D as component};
