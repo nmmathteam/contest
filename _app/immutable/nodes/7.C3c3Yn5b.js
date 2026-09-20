207f7037596baaf166dcf0953ab8f233ffe4bb6a
euiryka <euiry.kim@gmail.com>
2026-09-19 22:16:59 -0600

    Updated team cards and contest end

---
+++ _app/immutable/nodes/7.C3c3Yn5b.js
@@ -0,0 +1,29 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/Ce03F-E-.js";import{t as A,a as i,f as N,l as n,m as r,s as t,n as w,F as P,e as C,$ as T,ab as s,ac as o}from"../chunks/DsGXEWVO.js";import{h as k}from"../chunks/8IkXwzSs.js";import{N as O}from"../chunks/X0x9MJhx.js";import{F as U}from"../chunks/BTUGf2By.js";import{H as q}from"../chunks/CzgGxmOP.js";import{s as D}from"../chunks/CkcYvhBg.js";/* empty css                */var I=N('<div class="card svelte-1t1yi4p"><div class="card-inner bg-slate-200 shadow-sm hover:shadow-xl svelte-1t1yi4p"><div class="ur-wrapper svelte-1t1yi4p"><div class="username text-gray-700 text-4xl svelte-1t1yi4p"> </div> <div class="role text-gray-500 text-lg"> </div></div> <div class="desc-grid svelte-1t1yi4p"><div class="desc-clip svelte-1t1yi4p"><div class="description text-gray-600 text-base svelte-1t1yi4p"><!></div></div></div></div></div>');function l(S,m){var c=I(),v=n(c),p=n(v),h=n(p),$=n(h,!0);r(h);var u=t(h,2),_=n(u,!0);r(u),r(p);var f=t(p,2),g=n(f),y=n(g),x=n(y);D(x,m,"default",{}),r(y),r(g),r(f),r(v),r(c),A(()=>{w($,m.name),w(_,m.role)}),i(S,c)}var j=N('<section id="mainheader" class="svelte-1xw8l7g"><!> <!></section> <section class="card-wrapper mt-20 svelte-1xw8l7g"><!> <!> <!> <!> <!> <!> <!> <!> <!></section> <!>',1);function X(S){var m=j();k("1xw8l7g",e=>{C(()=>{T.title="NM³ | Our Team"})});var c=P(m),v=n(c);O(v,{clr:"dark"});var p=t(v,2);q(p,{title:"Our Team"}),r(c);var h=t(c,2),$=n(h);l($,{name:"Euiryeon Kim",role:"Director",children:(e,d)=>{s();var a=o(`Euiryeon (Aiden) Kim is a rising senior at La Cueva High School.
+        He has competed in many math and science competitions. He qualified to MATHCOUNTS
+        Nationals twice and qualified for VEX Robotics Worlds in high school. In his free time,
+        he listens to music, plays games, and watches films.`);i(e,a)},$$slots:{default:!0}});var u=t($,2);l(u,{name:"Grace Hsieh",role:"Lead Writer",children:(e,d)=>{s();var a=o(`Grace Hsieh is going into senior year at La Cueva
+        High School. Her favorite math concepts are knots, calculus, geometry,
+        and algebra. SHe went to Mathcounts Nationals in 2023 and has participated
+        in UNM-PNM since 2020 in addition to coaching MATHCOUNTS at Desert Ridge MS
+        and the Math Bashers club for elementary schoolers in Albuquerque. In her
+        free time, she likes to play piano, do crosswords, read, and draw.`);i(e,a)},$$slots:{default:!0}});var _=t(u,2);l(_,{name:"Ada Dyer",role:"Staff",children:(e,d)=>{s();var a=o(`Ada is a freshman at UNM majoring in mathematics and german.
+        She wrote problems for NM³ in 2025, and currently co-teaches
+        virtual computer science and math classes for middle schoolers
+        with Oak Ridge Computer Science Girls. When she's not studying,
+        she enjoys baking, practicing violin, and napping.`);i(e,a)},$$slots:{default:!0}});var f=t(_,2);l(f,{name:"Alicia Peng",role:"Staff",children:(e,d)=>{s();var a=o("Coming soon!");i(e,a)},$$slots:{default:!0}});var g=t(f,2);l(g,{name:"Ashvita Prasankumar",role:"Staff",children:(e,d)=>{s();var a=o(`Ashvita (Ashi) Prasankumar is a freshman at La Cueva High School.
+        She loves math and has participated in many different math competitions.
+        In her free time, she loves reading, swimming, tennis, and playing the
+        violin.`);i(e,a)},$$slots:{default:!0}});var y=t(g,2);l(y,{name:"Daniel Choi",role:"Staff",children:(e,d)=>{s();var a=o(`Daniel Choi is a sophomore at Bosque School. This is his second year helping with 
+        the contest, and he is excited to be involved again. Outside of math, Daniel enjoys playing 
+        piano and fencing.`);i(e,a)},$$slots:{default:!0}});var x=t(y,2);l(x,{name:"Iris Huang",role:"Staff",children:(e,d)=>{s();var a=o(`Iris Huang is a high school freshman at Albuquerque Academy. She's gone
+        to MATHCOUNTS Nationals three times (the calculator plushies were the best part,
+        if you ask her), plus she participates in and enjoys math competitions, like, a lot.
+        In her free time, you can probably find her reading, handcrafting, playing video games,
+        or, well, doing math.`);i(e,a)},$$slots:{default:!0}});var H=t(x,2);l(H,{name:"Alex Peng",role:"Staff",children:(e,d)=>{s();var a=o(`Alexander Peng is a freshman at La Cueva High School. He has participated in various 
+        math, science, and Robotics competitions, like the MATHCOUNTS Nationals competition, 
+        UNM-PNM math competition, VEX Robotics Worlds competition, and more. During his free time, 
+        Alex enjoys playing games, coding, and playing the piano and violin. Additionally, Mrs. Williams 
+        is his favorite teacher.`);i(e,a)},$$slots:{default:!0}});var b=t(H,2);l(b,{name:"Sean Choi",role:"Founder & Former Director",children:(e,d)=>{s();var a=o(`Sean Choi is a recent graduate of Stanford Online High School and is currently taking
+        a gap year before college. He serves as a coach and problem writer for the UNM-PNM contest.
+        He is also a 5-time AIME qualifier and a UNM-PNM winner. Outside of math, Sean enjoys
+        playing the piano, reading philosophy, and watching soccer.`);i(e,a)},$$slots:{default:!0}}),r(h);var M=t(h,2);U(M),i(S,m)}export{X as component};
